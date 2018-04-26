64af926765f227efa1fde4c34000a305667385e9
wmsj100 <2457782267@qq.com>
2018-04-26 15:26:51 +0800

    注释语句

---
+++ testcodes/Classes/view.swift
@@ -0,0 +1,9 @@
+//
+//  view.swift
+//  testcodes
+//
+//  Created by Cuicui You 尤翠翠 on 2018/4/26.
+//  Copyright © 2018年 Cuicui You 尤翠翠. All rights reserved.
+//
+
+import Foundation
---
+++ testcodes/testcodes.h
@@ -0,0 +1,19 @@
+//
+//  testcodes.h
+//  testcodes
+//
+//  Created by Cuicui You 尤翠翠 on 2018/4/26.
+//  Copyright © 2018年 Cuicui You 尤翠翠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testcodes.
+FOUNDATION_EXPORT double testcodesVersionNumber;
+
+//! Project version string for testcodes.
+FOUNDATION_EXPORT const unsigned char testcodesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testcodes/PublicHeader.h>
+
+
